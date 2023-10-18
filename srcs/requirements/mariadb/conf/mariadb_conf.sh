38e65df7835dfe91886b8de381b03b502a8bda0f
LeonPupier <leonpupier@gmail.com>
2023-10-18 16:42:03 +0200

    ad docker compose

--- srcs/requirements/mariadb/conf/mariadb_conf.sh
+++ srcs/requirements/mariadb/conf/mariadb_conf.sh
@@ -5,7 +5,7 @@ set -e
 # Start MySQL service
 service mysql start
 
-if [ ! -d "/var/lib/mysql/SQL_DATABASE" ]
+if [ ! -d "/var/lib/mysql/$SQL_DATABASE" ]
 
 then
 
@@ -20,24 +20,24 @@ y
 EOF
 
 # Create the SQL_DATABASE database
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS SQL_DATABASE;"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
 
 # Create the SQL_USER with the SQL_PASSWORD password
-mysql -uroot -e "CREATE USER IF NOT EXISTS SQL_USER@'%' IDENTIFIED BY 'SQL_PASSWORD';"
+mysql -uroot -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 
 # Give rights to SQL_USER on SQL_DATABASE
-mysql -uroot -e "GRANT ALL PRIVILEGES ON SQL_DATABASE.* TO 'SQL_USER'@'%';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';"
 
 # Update the configuration of MySQL
 mysql -uroot -e "FLUSH PRIVILEGES;"
 
 # Change root rights with the SQL_ROOT_PASSWORD password
-mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'SQL_ROOT_PASSWORD';"
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
 
 fi
 
 # Shudown MySQL
-mysqladmin -u root -pSQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
 # Restart MySQL with the new configuration
 exec "$@"
