6199efb046a03cf28a9fa0dc5ec92fec00c3d4c4
LeonPupier <leonpupier@gmail.com>
2023-10-18 15:47:50 +0200

    WordPress finished

--- srcs/requirements/mariadb/conf/mariadb_conf.sh
+++ srcs/requirements/mariadb/conf/mariadb_conf.sh
@@ -1,23 +1,43 @@
+#!/bin/bash
+
+set -e
+
 # Start MySQL service
-service mysql start;
+service mysql start
+
+if [ ! -d "/var/lib/mysql/SQL_DATABASE" ]
+
+then
+
+# Secure install
+mysql_secure_installation << EOF
+
+n
+y
+y
+y
+y
+EOF
 
 # Create the SQL_DATABASE database
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS SQL_DATABASE;"
 
 # Create the SQL_USER with the SQL_PASSWORD password
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -uroot -e "CREATE USER IF NOT EXISTS SQL_USER@'%' IDENTIFIED BY 'SQL_PASSWORD';"
 
-# Give rights to SQL_USER with the password SQL_PASSWORD on SQL_DATABASE
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-# Change root rights with the SQL_ROOT_PASSWORD password
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# Give rights to SQL_USER on SQL_DATABASE
+mysql -uroot -e "GRANT ALL PRIVILEGES ON SQL_DATABASE.* TO 'SQL_USER'@'%';"
 
 # Update the configuration of MySQL
-mysql -e "FLUSH PRIVILEGES;"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+
+# Change root rights with the SQL_ROOT_PASSWORD password
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'SQL_ROOT_PASSWORD';"
+
+fi
 
 # Shudown MySQL
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -pSQL_ROOT_PASSWORD shutdown
 
 # Restart MySQL with the new configuration
-exec mysqld_safe
+exec "$@"
